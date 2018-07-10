2b56f4027988612ac1f59d676a2bc4a5796149f7
John Jung <jej@moss.lib.uchicago.edu>
2018-07-10 13:25:59 -0500

    Refactor code, add tests.

---
+++ test.py
@@ -0,0 +1,51 @@
+import json
+import unittest
+import urllib.request
+
+from mvol_collection_year import IIIFCollectionYear
+from mvol_collection_month import IIIFCollectionMonth
+
+def ordered(obj):
+  if isinstance(obj, dict):
+    return sorted((k, ordered(v)) for k, v in obj.items())
+  if isinstance(obj, list):
+    return sorted(ordered(x) for x in obj)
+  else:
+    return obj
+
+class TestIIIFTools(unittest.TestCase):
+
+  def test_iiif_collection_year(self): 
+    url = 'http://iiif-collection.lib.uchicago.edu/mvol/0004/mvol-0004-1930.json'
+    live_data = json.load(urllib.request.urlopen(url))
+    test_data = IIIFCollectionYear(
+      'Daily Maroon',
+      'mvol-0004-1930',
+      'A newspaper produced by students of the University of Chicago. Published 1900-1942 and continued by the Chicago Maroon.',
+      'University of Chicago',
+      '/Volumes/webdav/IIIF_Files/mvol/0004/1930'
+    ).data()
+    self.assertTrue(ordered(live_data) == ordered(test_data))
+
+  def test_iiif_collection_month(self):
+    url = 'http://iiif-collection.lib.uchicago.edu/mvol/0004/mvol-0004-1930-01.json'
+    live_data = json.load(urllib.request.urlopen(url))
+    test_data = IIIFCollectionMonth(
+      'Daily Maroon',
+      'mvol-0004-1930-01',
+      'A newspaper produced by students of the University of Chicago. Published 1900-1942 and continued by the Chicago Maroon.',
+      'University of Chicago',
+      '/Volumes/webdav/IIIF_Files/mvol/0004/1930'
+    ).data()
+    self.assertTrue(ordered(live_data) == ordered(test_data))
+
+  '''
+  def test_iiif_manifest(self):
+    url = 'http://iiif-manifest.lib.uchicago.edu/mvol/0004/1930/0106/mvol-0004-1930-0106.json'
+    live_data = json.load(urllib.request.urlopen(url).read())
+    test_data = {}
+    self.assertTrue(ordered(live_data) == ordered(test_data))
+  '''
+
+if __name__ == '__main__':
+  unittest.main()
