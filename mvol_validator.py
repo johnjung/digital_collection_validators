50dadf15abc4906361d74e5813c06ad64eb4ce41
John Jung <jej@moss.lib.uchicago.edu>
2018-07-24 10:49:16 -0500

    First round of work on validation script.

--- mvol_validator.py
+++ mvol_validator.py
@@ -17,15 +17,16 @@ def validate_year_folder(oc, year_path):
   if year_path[-1:] == '/':
     pieces.pop()
 
-  if not re.match('^.*[/]\d{4}[/]$', year_path):
+  if not re.match('^.*[/]\d{4}$', year_path):
     errors.append(year_path + ' is not a valid year.')
 
   for entry in oc.list(year_path):
-    if not entry.file_type == 'dir':
-      errors.append(year_path + ' contains entries that are not folders.')
-
-    if not re.match('^.*[/]\d{4}[/]$', entry.path):
-      errors.append(year_path + ' contains entries that are not mmdd.')
+    if entry.file_type == 'dir':
+      if not re.match('^.*[/]\d{4}[/]$', entry.path):
+        errors.append(year_path + ' contains folders that are not mmdd.')
+    else:
+      if not re.match('^.*\.csv$', entry.path):
+        errors.append(year_path + ' contains files that are not .csv.')
 
   return errors
 
@@ -37,47 +38,69 @@ def validate_date_folder(oc, date_path):
 
   errors = []
 
-  pieces = entry.path.split('/')
+  pieces = date_path.split('/')
   pieces.pop()
   month_date = pieces.pop()
 
   if int(month_date[:2]) < 1:
-    errors.append(entry.path + ' is not a valid date.')
+    errors.append(date_path + ' is not a valid date.')
 
   if int(month_date[:2]) > 12:
-    errors.append(entry.path + ' is not a valid date.')
+    errors.append(date_path + ' is not a valid date.')
 
   if int(month_date[2:4]) < 1:
-    errors.append(entry.path + ' is not a valid date.')
+    errors.append(date_path + ' is not a valid date.')
 
   if int(month_date[2:4]) > 31:
-    errors.append(entry.path + ' is not a valid date.')
+    errors.append(date_path + ' is not a valid date.')
 
   return errors
 
-def validate_alto_folder(date_path):
-  pass
-  # ALTO exists and is a directory.
-  # for each thing in ALTO:
-  # it is a file.
-  # it is in the form identifier_\d{4}.xml
-  # it contains some content. 
+def get_identifier(path):
+  pieces = entry.path.split('/')
+  if entry.path[-1:] == '/':
+    pieces.pop()
+  entry_filename = pieces.pop()
+  
+  identifier = []
+
+  while True:
+    piece = pieces.pop()
+    if piece == 'mvol':
+      identifier = [piece] + pieces[0:1]
+
+  return '-'.join(identifier)
+   
+def validate_folder(oc, date_path, folder_type, extension):
+  errors = []
+
+  folder_exists = False
+  for entry in oc.list(date_path):
+    if '/' + folder_type + '/' in entry.path and entry.file_type == 'dir':
+      folder_exists = True
+
+  if folder_exists:
+    for entry in oc.list(date_path + '/' + folder_type):
+      if not entry.file_type == 'file':
+        errors.append(date_path + '/' + folder_type + '/ contains things that are not files.')
+
+      identifier_match = get_identifier(entry.path).replace('-', '\-')
+   
+      if not re.match(identifier_match + '\d{4}\.' + extension + '$', entry.path):
+        errors.append(date_math + '/' + folder_type + ' contains incorrectly named files.')
+  else:
+    errors.append(date_path + ' does not include an ALTO directory.')
+
+  return errors
+
+def validate_alto_folder(oc, date_path):
+  return validate_folder(oc, date_path, 'ALTO', 'xml')
 
 def validate_jpeg_folder(date_path):
-  pass
-  # JPEG exists and is a directory.
-  # for each thing in JEPG:
-  # it is a file.
-  # it is in the form identifier_\d{4}.jpg.
-  # it is a jpeg.
+  return validate_folders(oc, date_path, 'JPEG', 'jpg')
 
 def validate_tiff_folder(date_path):
-  pass
-  # TIFF exists and is a directory.
-  # for each thing in TIFF:
-  # it is a file.
-  # it is in the form identifier_\d{4}.tif.
-  # it is a tiff.
+  return validate_folders(oc, date_path, 'TIFF', 'tif')
 
 def validate_dc_xml(date_path):
   pass
@@ -145,14 +168,12 @@ if __name__ == '__main__':
 
   errors = []
 
-  errors = errors + validate_year_folder(args.directory)
+  errors = errors + validate_year_folder(oc, args.directory)
 
-  date_folders = []
   for date_folder in oc.list(args.directory):
-    pieces = date_folder.path.split('/')
-    if date_folder[-1:] == '/':
-      pieces.pop()
-    date_folders.append(pieces.pop())
-  errors = errors + validate_date_folders(date_folders)
-  
+    if date_folder.file_type == 'dir':
+      errors = errors + validate_date_folder(oc, date_folder.path)
 
+  for e in errors:
+    print(e)
+  
