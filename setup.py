1cb5b40b74cbf409fb01f3f6da104999dbacac07
John Jung <jej@s4.lib.uchicago.edu>
2020-11-02 10:39:06 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    author='John Jung',
+    author_email='jej@uchicago.edu',
+    description='Scripts to validate digital collections before converting them to OCFL',
+    install_requires=[
+        'cryptography==2.5.0',
+        'docopt',
+        'lxml',
+        'paramiko',
+        'Pillow'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name='Digital Collection Validators',
+    packages=setuptools.find_packages(),
+    url='https://github.com/johnjung/digital_collection_validators',
+    version='0.0.1'
+)
